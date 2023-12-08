7e04896355def7ac7f328477e8d41636c73cc2c6
jiacheng <jiacheng.li@thoughtworks.com>
2023-12-08 16:59:20 +0800

    chore: first commit

---
+++ src/main/java/com/example/conductordemo/ConductorDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.conductordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConductorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConductorDemoApplication.class, args);
+    }
+}
